5d86070c020b5b242a7d3b7bd4adba07436e67f7
张策 <zhangce@kuaiqiangche.com>
2016-08-24 17:06:21 +0800

    ok

---
+++ PodTestOne/PodTestOne/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PodTestOne
+//
+//  Created by 张策 on 16/8/24.
+//  Copyright © 2016年 张策. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PodTestOne/PodTestOneTests/PodTestOneTests.m
@@ -0,0 +1,39 @@
+//
+//  PodTestOneTests.m
+//  PodTestOneTests
+//
+//  Created by 张策 on 16/8/24.
+//  Copyright © 2016年 张策. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PodTestOneTests : XCTestCase
+
+@end
+
+@implementation PodTestOneTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
